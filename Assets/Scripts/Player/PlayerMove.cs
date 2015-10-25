b8e418e7d2badfa1bde56b53e5013f61704c6ff9
JustBeYou <mihailferaru2000@gmail.com>
2015-10-25 12:10:25 +0200

    Added player movement script.

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	// Speed and gravity of player
+	public float moveSpeed;
+	public float playerRotateSpeed;
+	public float cameraRotateSpeed;
+	public float jumpSpeed;
+	public float gravity;
+
+	// Movement vector
+	private Vector3 move;
+
+	// Rotate vector
+	private Vector3 playerRotate;
+	private Vector3 cameraRotate;
+
+	// Camera rotate range
+	private float cameraRotateRange = 60.0f;
+
+	// Rigidbody and Character Controller
+	private CharacterController cc;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		// Lock cursor
+		Screen.lockCursor = true;
+
+		// Get components
+		cc = GetComponent<CharacterController> ();
+		rb = GetComponent<Rigidbody> ();
+
+		// Initialize gravity
+		gravity = rb.mass;
+
+		// Initialize vectors
+		move = Vector3.zero;
+		playerRotate = Vector3.zero;
+		cameraRotate = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Rotation
+		float horizontalRotate = Input.GetAxis ("Mouse X");
+		float verticalRotate = cameraRotate.x - Input.GetAxis ("Mouse Y")  * cameraRotateSpeed;
+		verticalRotate = Mathf.Clamp (verticalRotate, -cameraRotateRange, cameraRotateRange);
+
+		playerRotate.x = 0.0F;
+		playerRotate.y = horizontalRotate;
+		playerRotate.z = 0.0F;
+
+		cameraRotate.x = verticalRotate;
+		cameraRotate.y = 0.0F;
+		cameraRotate.z = 0.0F;
+
+		transform.Rotate (playerRotate * playerRotateSpeed);
+		Camera.main.transform.localRotation = Quaternion.Euler (cameraRotate);
+
+		// Movement
+		float verticalMove = Input.GetAxis ("Vertical");
+		float horizontalMove = Input.GetAxis ("Horizontal");
+
+		move.x = horizontalMove;
+		move.y = 0.0F;
+		move.z = verticalMove;
+
+		move = transform.rotation * move;
+
+		cc.SimpleMove (move * moveSpeed);
+	}
+}
